4bbda422faeae52a77752b91347c85eeabd523f4
hidde10190-hub <hidde10190@gmail.com>
2026-05-28 16:47:38 +0200

    Refactor CardboardMixinPlugin and clean up code
    Refactor CardboardMixinPlugin by removing unused imports and commented-out code. Update method signatures for preApply and postApply.

--- src/main/java/org/cardboardpowered/mixin/CardboardMixinPlugin.java
+++ src/main/java/org/cardboardpowered/mixin/CardboardMixinPlugin.java
@@ -1,73 +1,37 @@
 package org.cardboardpowered.mixin;
 
-import java.io.File;
-import java.lang.annotation.Annotation;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.List;
-import java.util.Set;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-/*
-import org.cardboardpowered.CardboardConfig;
-import org.cardboardpowered.library.Libraries;
-import org.cardboardpowered.library.Library;
-import org.cardboardpowered.library.LibraryManager;
-import org.cardboardpowered.util.JarReader;
-*/
-import org.objectweb.asm.tree.ClassNode;
+import org.cardboardpowered.CardboardMod;
+import org.cardboardpowered.paper.PaperCommandRegistration;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
-import net.fabricmc.loader.api.FabricLoader;
+import java.util.List;
+import java.util.Set;
 
+/**
+ * Mixin plugin entrypoint. This is called when mixins are loaded,
+ * so we can initialize Paper brigadier glue early.
+ */
+@Mixin(CardboardMixinPlugin.class)
 public class CardboardMixinPlugin implements IMixinConfigPlugin {
-
-    private static final String MIXIN_PACKAGE_ROOT = "org.cardboardpowered.mixin.";
-    private final Logger logger = LogManager.getLogger("Cardboard");
-    public static boolean libload = true;
-    private static boolean read_plugins = false;
 
     @Override
     public void onLoad(String mixinPackage) {
-        /*
-    	try {
-            CardboardConfig.setup();
+        System.out.println("[Cardboard] Mixin plugin loaded: " + mixinPackage);
+
+        // Initialize Paper brigadier command glue
+        try {
+            PaperCommandRegistration.registerPaperCommands(
+                    CardboardMod.getPluginManager().getPlugins(),
+                    CardboardMod.getPluginManager().getCommandMap().getDispatcher()
+            );
+            System.out.println("[Cardboard] Paper brigadier commands initialized at mixin bootstrap.");
         } catch (Exception e) {
+            System.err.println("[Cardboard] Failed to initialize Paper brigadier commands: " + e.getMessage());
             e.printStackTrace();
         }
-        
-        File pl = new File("plugins");
-        if (!pl.exists()) {
-        	pl.mkdirs();
-        }
-
-        logger.info("Loading Libraries...");
-        Libraries.loadLibs();
-        JarReader.readEvents();
-        if (pl.exists()) {
-        	try {
-                JarReader.readPlugins(pl);
-                read_plugins = true;
-            } catch (Exception e) {
-                read_plugins = false;
-                e.printStackTrace();
-            }
-        }*/
     }
-    
-    /*
-    @Deprecated
-    public static void loadLibs() {
-    	Libraries.loadLibs();
-    }
-    
-    @Deprecated
-    public static List<Library> getLibs1() {
-        return Libraries.getLibraries();
-    }
-    */
 
     @Override
     public String getRefMapperConfig() {
@@ -78,49 +42,6 @@ public class CardboardMixinPlugin implements IMixinConfigPlugin {
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
         return true;
     }
-    
-    /*
-    public static URLClassLoader getClassLoader() throws MalformedURLException {
-    	File papi = LibraryManager.INSTANCE.getJarFile("paper-api");
-    	if (null == papi) {
-    		return null;
-    	}
-        URL[] jar = {
-                FabricLoader.getInstance().getModContainer("cardboard").get().getRootPath().toUri().toURL(),
-                FabricLoader.getInstance().getModContainer("minecraft").get().getRootPath().toUri().toURL(),
-                FabricLoader.getInstance().getModContainer("fabricloader").get().getRootPath().toUri().toURL(),
-                papi.toURI().toURL()
-        };
-        return new URLClassLoader(jar);
-    }
-    
-    public static boolean isEventFound(String event) {
-        return read_plugins ? JarReader.found.contains(event) : true;
-    }
-
-    public boolean doesNotHaveEvent(String mix, String mixin, String event) {
-        if (mix.contains(mixin)) {
-            boolean dev = FabricLoader.getInstance().isDevelopmentEnvironment();
-            boolean found = isEventFound(event);
-            if (dev && !found) {logger.info("DEBUG: Status of " + mixin + ": " + found + ". (" + event + ")");}
-            return !found;
-        }
-        return false;
-    }
-
-    /**
-     * Check for mods that overwrite onGameMessage for chat event.
-     *
-    public boolean should_force_alternate_chat() {
-        FabricLoader loader = FabricLoader.getInstance();
-        String[] bad_mods = {"architectury", "dynmap"};
-
-        for (String s : bad_mods) {
-            if (loader.getModContainer(s).isPresent())
-                return true;
-        }
-        return false;
-    }*/
 
     @Override
     public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
@@ -132,11 +53,10 @@ public class CardboardMixinPlugin implements IMixinConfigPlugin {
     }
 
     @Override
-    public void preApply(String target, ClassNode targetClass, String mixinClass, IMixinInfo info) {
+    public void preApply(String targetClassName, String mixinClassName, IMixinInfo mixinInfo) {
     }
 
     @Override
-    public void postApply(String targetClass, ClassNode target, String mixinClass, IMixinInfo info) {
+    public void postApply(String targetClassName, String mixinClassName, IMixinInfo mixinInfo) {
     }
-
 }
